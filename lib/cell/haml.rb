3fa07befdbc07cb0ad66107047aa75696f8f9c2a
Abdelkader Boudih <terminale@gmail.com>
2015-01-24 11:09:26 +0000

    Update haml.rb

--- lib/cell/haml.rb
+++ lib/cell/haml.rb
@@ -59,7 +59,7 @@ module Cell
     # From FormTagHelper. why do they escape every possible string? why?
     def form_tag_in_block(html_options, &block)
       content = capture(&block)
-      "#{form_tag_html(html_options)}" << content << "</form>"
+      form_tag_with_body(html_options, content)
     end
 
     def form_tag_with_body(html_options, content)
